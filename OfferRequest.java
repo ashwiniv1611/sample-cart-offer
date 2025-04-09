493af3f8706b1d3f76da5fa8abee814dc5310e3c
ashwiniv1611 <127727535+ashwiniv1611@users.noreply.github.com>
2025-04-10 01:42:52 +0530

    Create OfferRequest.java

---
+++ OfferRequest.java
@@ -0,0 +1,19 @@
+package com.zomato.offerapi.model;
+public class OfferRequest {
+private String segment;
+private double totalAmount;
+// Getters and Setters
+public String getSegment() {
+return segment;
+
+}
+public void setSegment(String segment) {
+this.segment = segment;
+}
+public double getTotalAmount() {
+return totalAmount;
+}
+public void setTotalAmount(double totalAmount) {
+this.totalAmount = totalAmount;
+}
+}
